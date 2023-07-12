ac64a1a8f7b37e123ae2e0373132c81b22f1af22
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2023-07-12 13:20:34 +0200

    refactor validation checks

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,62 +1,34 @@
 function ConvertHandler() {
   this.getNum = function (input) {
-    console.log("input", input);
-    if (input === "") return 1;
     if (!/\d/.test(input)) return 1;
     let result = input.match(/^(\d+(\.\d+){0,1}(\/\d+(\.\d+){0,1}){0,1})[A-Za-z]*$/);
-    if (!result) return null;
-    if (result[1].includes("/")) {
-      let [op1, op2] = result[1].split("/");
-      return Number(op1) / Number(op2);
-    } else {
-      return Number(result[1]);
-    }
+    if (!result) return undefined;
+    let [op1, op2 = 1] = result[1].split("/");
+    return parseFloat((Number(op1) / Number(op2)).toFixed(5));
   };
 
   this.getUnit = function (input) {
-    let result = input.match(/(gal|l|lbs|kg|mi|kg)$/i);
-    return result[1] === "l" || result[1] === "L" ? result[1].toUpperCase() : result[1].toLowerCase();
+    let result = input.match(/(gal|l|lbs|kg|mi|km)$/i)?.[1]?.toLowerCase();
+    return result && (result === "l" ? "L" : result);
   };
 
   this.getReturnUnit = function (initUnit) {
-    if (initUnit === "gal") return "L";
-    if (initUnit === "L") return "gal";
-    if (initUnit === "lbs") return "kg";
-    if (initUnit === "kg") return "lbs";
-    if (initUnit === "mi") return "km";
-    if (initUnit === "km") return "mi";
+    let initToReturnUnit = { gal: "L", L: "gal", lbs: "kg", kg: "lbs", mi: "km", km: "mi" };
+    return initToReturnUnit[initUnit];
   };
 
-  this.spellOutUnit = function (initNum, initUnit, returnNum, returnUnit) {
-    console.log("hi");
-    let dict = {
-      gal: "gallons",
-      L: "liters",
-      lbs: "pounds",
-      kg: "kilograms",
-      mi: "miles",
-      km: "kilometers",
-    };
-    let result = `${initNum} ${dict[initUnit]} converts to ${parseFloat(returnNum.toFixed(5))} ${dict[returnUnit]}`;
-    console.log("spellOut===========>", result);
-    return result;
+  this.spellOutUnit = function (unit) {
+    let fullNames = { gal: "gallons", L: "liters", lbs: "pounds", kg: "kilograms", mi: "miles", km: "kilometers" };
+    return fullNames[unit];
   };
 
   this.convert = function (initNum, initUnit) {
-    let result;
-    if (initUnit === "gal") result = initNum * 3.78541;
-    if (initUnit === "L") result = initNum * 0.26417;
-    if (initUnit === "lbs") result = initNum * 0.453592;
-    if (initUnit === "kg") result = initNum * 2.20462;
-    if (initUnit === "mi") result = initNum * 1.60934;
-    if (initUnit === "km") result = initNum * 0.62137;
-    return result;
+    let convRates = { gal: 3.78541, L: 1 / 3.78541, lbs: 0.453592, kg: 1 / 0.453592, mi: 1.60934, km: 1 / 1.60934 };
+    return parseFloat((initNum * convRates[initUnit]).toFixed(5));
   };
 
-  this.getString = function (initNum, initUnit, returnNum, returnUnit, spellOutUnit) {
-    let result = `{"initNum":${initNum},"initUnit":"${initUnit}","returnNum":${parseFloat(returnNum.toFixed(5))},"returnUnit":"${returnUnit}","string":"${spellOutUnit}"}`;
-    console.log("toString===========>", result);
-    return result;
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
 }
 
--- routes/api.js
+++ routes/api.js
@@ -9,22 +9,17 @@ module.exports = function (app) {
     let input = req.query.input;
     let initNum = convertHandler.getNum(input);
     let initUnit = convertHandler.getUnit(input);
-    let returnNum = convertHandler.convert(initNum, initUnit);
-    let returnUnit = convertHandler.getReturnUnit(initUnit);
-    console.log("initNum", initNum, "initUnit", initUnit, "returnNum", returnNum, "returnUnit", returnUnit);
-    let spellOutUnit = convertHandler.spellOutUnit(initNum, initUnit, returnNum, returnUnit);
-    let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit, spellOutUnit);
-    console.log("hiiiiii");
-    // res.send(spellOutUnit);
-    res.send(toString);
-    // if (!initNum && !initUnit) {
-    //   res.send("invalid number and unit");
-    // } else if (!initNum) {
-    //   res.send("invalid number");
-    // } else if (!initUnit) {
-    //   res.send("invalid unit");
-    // } else {
-    //   res.send(toString);
-    // }
+    if (!initNum && !initUnit) {
+      res.send("invalid number and unit");
+    } else if (!initNum) {
+      res.send("invalid number");
+    } else if (!initUnit) {
+      res.send("invalid unit");
+    } else {
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      res.json({ initNum, initUnit, returnNum, returnUnit, string: toString });
+    }
   });
 };
